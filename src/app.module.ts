503a56e05a007e6d76e3bdb8cf18b4673321c73f
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-09-09 10:17:47 +0700

    chore: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { MerchantModule } from './merchant/merchant.module';
 import { PointModule } from './point/point.module';
+import { ConfigModule } from '@nestjs/config';
+import { configSchema } from './configSchema';
 
 @Module({
-  imports: [PrismaModule, UserModule, MerchantModule, PointModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    MerchantModule,
+    PointModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configSchema.ts
@@ -0,0 +1,7 @@
+import * as Joi from 'joi';
+
+export const configSchema = Joi.object({
+  POINT_FACTORY_ADDRESS: Joi.string(),
+  PRIVATE_KEY: Joi.string(),
+  RPC_URL: Joi.string(),
+});

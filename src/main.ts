f2643b9a2f9794fd60a6697185d9c4dccdc584a2
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-09-13 11:14:36 +0700

    config: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.enableCors();
+
   await app.listen(4000);
 }
 bootstrap();

f3b5bf1092c2cb07458df48fc74cf9a48865a669
Paramet Kongjaroen <prame@MacBook-Air-khxng-Paramet.local>
2024-09-17 14:32:04 +0700

    fix: reduce dto

--- src/modules/point/dtos/index.ts
+++ src/modules/point/dtos/index.ts
@@ -1,3 +1,55 @@
-export * from './get-points.dto';
-export * from './create-point.dto';
-export * from './redeem-point.dto';
+import { IsNumber, IsString } from 'class-validator';
+
+export class GetPointDto {
+  @IsString()
+  pointId: string;
+}
+
+export class GetPointsDto {
+  @IsString()
+  merchantId: string;
+}
+export class CreatePointDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  symbol: string;
+
+  @IsString()
+  merchantId: string;
+
+  @IsNumber()
+  initialSupply: number;
+
+  @IsNumber()
+  decimal: number;
+
+  @IsNumber()
+  frameSize: number;
+
+  @IsNumber()
+  slotSize: number;
+}
+
+export class RedeemPointsDto {
+  @IsString()
+  merchantId: string;
+
+  @IsString()
+  pointId: string;
+}
+
+export class RedeemPointBodyDto {
+  @IsString()
+  transactionTypeId: string;
+
+  @IsString()
+  receiverAddress: string;
+
+  @IsNumber()
+  amount: number;
+
+  @IsString()
+  email: string;
+}

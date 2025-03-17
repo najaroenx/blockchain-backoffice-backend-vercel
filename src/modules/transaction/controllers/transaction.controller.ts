46b4d621887f8e0592e1d62820610024f1c50d92
paramet kongjaroen <parametprame2@gmail.com>
2025-03-17 14:00:05 +0700

    feat: add api mint and burn tokens

--- src/modules/transaction/controllers/transaction.controller.ts
+++ src/modules/transaction/controllers/transaction.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { CreateTransactionBodyDto, CreateTransactionC2CBodyDto } from '../dtos';
+import {
+  CreateBurnTransactionBodyDto,
+  CreateMintTransactionBodyDto,
+  CreateTransactionBodyDto,
+  CreateTransactionC2CBodyDto,
+} from '../dtos';
 import { createBufferFromHex } from 'src/libs/createBufferFromHex';
 import { GetTransactionsByCustomerId } from '../handlers/getTransactionsByCustomerId.handler';
 import { GetTransactionsByMerchantId } from '../handlers/getTransactionsByMerchantId.handler';
 import { CreateTransactionB2C } from '../handlers/createTransactionB2C.handler';
 import { CreateTransactionC2C } from '../handlers/createTransactionC2C.handler';
-
+import { MintTransaction } from '../handlers/mintTransaction.handler';
+import { BurnTransaction } from '../handlers/burnTransaction.handler';
 @Controller('/:merchantId/transaction')
 export class TransactionController {
   constructor(
@@ -13,6 +19,8 @@ export class TransactionController {
     private readonly getTransactionsByMerchantId: GetTransactionsByMerchantId,
     private readonly createTransactionB2C: CreateTransactionB2C,
     private readonly createTransactionC2C: CreateTransactionC2C,
+    private readonly mintTransaction: MintTransaction,
+    private readonly burnTransaction: BurnTransaction,
   ) {}
 
   @Get('/')
@@ -51,6 +59,31 @@ export class TransactionController {
     @Body() body: CreateTransactionC2CBodyDto,
   ) {
     return this.createTransactionC2C.execute(merchantId, pointId, {
+      ...body,
+    });
+  }
+
+  @Post('/:pointId/mint')
+  @HttpCode(201)
+  async mintTokens(
+    @Param('merchantId') merchantId: string,
+    @Param('pointId') pointId: string,
+    @Body() body: CreateMintTransactionBodyDto,
+  ) {
+    return this.mintTransaction.execute(merchantId, pointId, {
+      ...body,
+      senderAddress: createBufferFromHex(body.senderAddress),
+    });
+  }
+
+  @Post('/:pointId/burn')
+  @HttpCode(201)
+  async burnTokens(
+    @Param('merchantId') merchantId: string,
+    @Param('pointId') pointId: string,
+    @Body() body: CreateBurnTransactionBodyDto,
+  ) {
+    return this.burnTransaction.execute(merchantId, pointId, {
       ...body,
     });
   }
